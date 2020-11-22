061d975dee9c09e09fefa76018e9abba110787fc
Browser Captures <browsercaptures@gmail.com>
2020-11-22 12:35:57 +0000

    Install python script

---
+++ installpython.sh
@@ -0,0 +1,7 @@
+# https://phoenixnap.com/kb/how-to-install-python-3-ubuntu
+
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update -y
+sudo apt-get install python3.8 -y
