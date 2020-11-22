e1b082fcb5c9257febef6e93092f93ebf071f3ad
Browser Captures <browsercaptures@gmail.com>
2020-11-22 10:53:52 +0000

    Stream games

--- mbook.js
+++ mbook.js
@@ -3,8 +3,13 @@ const MongoClient = require('mongodb').MongoClient
 const MONGODB_URI = process.env.MONGODB_URI
  
 const dbName = 'games'
+
+const { streamNdjson } = require('./fetchutils.js')
+
+const BOT_NAME = process.env.BOT_NAME || "chesshyperbot"
+const BOT_TOKEN = process.env.BOT_TOKEN
  
-MongoClient.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {  
+/*MongoClient.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {  
 	if(err){
 		console.log("MongoDb connection failed.")
 	}else{
@@ -13,5 +18,14 @@ MongoClient.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: tru
 		const db = client.db(dbName)
  
   		client.close()
+	}
+})*/
+
+streamNdjson({
+	url: `https://lichess.org/api/games/user/${BOT_NAME}?max=10`,
+	token: BOT_TOKEN,
+	log: true,
+	callback: blob => {
+		console.log(blob)
 	}
 })
